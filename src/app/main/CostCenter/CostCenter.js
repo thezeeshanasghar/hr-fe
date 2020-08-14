7c7697c8791bcb6da7be3ab8cb60e4531f964417
Naeem_Abbas <naeemabbasa25@gmail.com>
2020-08-14 17:10:42 +0500

    cost center

--- src/app/main/CostCenter/CostCenter.js
+++ src/app/main/CostCenter/CostCenter.js
@@ -18,6 +18,15 @@ import EditIcon from '@material-ui/icons/Edit';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Icon, Input, MuiThemeProvider} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import SimpleReactValidator from 'simple-react-validator';
+import axios from "axios";
+import toastr from 'toastr';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+
 const styles = theme => ({
 	container: {
 		display: 'flex',
@@ -34,6 +43,10 @@ const styles = theme => ({
 	menu: {
 		width: 200,
 	},
+	formControl: {
+		margin: theme.spacing.unit,
+		minWidth: "99%",
+	}
 });
 
 const CustomTableCell = withStyles(theme => ({
@@ -66,9 +79,167 @@ const rows = [
 class CostCenter extends Component {
 	state = {
 		value: 0,
+		company: "",
+		Companies: [],
+		costcenter:[],
+		code:"",
+		description:"",
+		Action:"Insert Record",
+		Id:0
 	};
-	handleChange = (event, value) => {
+	constructor(props) {
+		super(props);
+		this.validator = new SimpleReactValidator();
+
+	}
+	componentDidMount() {
+		this.getCompanies();
+		this.getCostCenter();
+	}
+	getCompanies = () => {
+		axios({
+			method: "get",
+			url: "http://localhost:3000/api/Company",
+			headers: {
+				// 'Authorization': `bearer ${token}`,
+				"Content-Type": "application/json;charset=utf-8",
+			},
+		})
+			.then((response) => {
+				console.log(response);
+				this.setState({ Companies: response.data });
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+	insertUpdateCostCenter = () => {
+		if (!this.validator.allValid()) {
+			this.validator.showMessages();
+			this.forceUpdate();
+		} else {
+			var method = "post";
+			var url = "http://localhost:3000/api/CostCenter";
+			if(this.state.Action !="Insert Record")
+			{
+				 method = "put";
+				 url = "http://localhost:3000/api/CostCenter/"+this.state.Id;
+			}
+			// console.log(this.state.company,this.state.employee,this.state.dateFrom,this.state.dateTo);
+			var obj = {
+				CompanyId: this.state.company,
+				Code: this.state.code,
+				Description: this.state.description
+			};
+			axios.interceptors.request.use(function (config) {
+				// document.getElementsByClassName("loader-wrapper")[0].style.display="block"
+				return config;
+			}, function (error) {
+				console.log('Error');
+				return Promise.reject(error);
+			});
+			axios({
+				method: method,
+				url: url,
+				data: JSON.stringify(obj),
+				headers: {
+					// 'Authorization': `bearer ${token}`,
+					"Content-Type": "application/json;charset=utf-8",
+				},
+			})
+				.then((response) => {
+					toastr.success('Operation successfull');
+					this.getCostCenter();
+					this.setState({
+						company: "",
+						code: "",
+						description: "",
+						Action:"Insert Record",
+						Id:0
+					});
+				})
+				.catch((error) => {
+					console.log(error);
+					toastr.error('Operation unsuccessfull');
+					this.setState({
+						company: "",
+						code: "",
+						description: "",
+						Action:"Insert Record",
+						Id:0
+					})
+				})
+
+
+		}
+	}
+	getCostCenter = () => {
+		axios({
+			method: "get",
+			url: "http://localhost:3000/api/CostCenter",
+			headers: {
+				// 'Authorization': `bearer ${token}`,
+				"Content-Type": "application/json;charset=utf-8",
+			},
+		})
+			.then((response) => {
+				console.log(response);
+				this.setState({ costcenter: response.data });
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+	getCostCenterById = (id) => {
+		axios({
+			method: "get",
+			url: "http://localhost:3000/api/CostCenter/" + id,
+			headers: {
+				// 'Authorization': `bearer ${token}`,
+				"Content-Type": "application/json;charset=utf-8",
+			},
+		})
+			.then((response) => {
+				console.log(response);
+				this.setState({
+					code: response.data[0].Code,
+					company: response.data[0].CompanyId,
+					description: response.data[0].Description,
+					value: 1,
+					Id:response.data[0].Id,
+					Action :"Update Record"
+				});
+
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+	deleteCostCenter=(id)=>{
+		axios({
+			method: "delete",
+			url: "http://localhost:3000/api/CostCenter/"+id,
+			headers: {
+			  // 'Authorization': `bearer ${token}`,
+			  "Content-Type": "application/json;charset=utf-8",
+			},
+		  })
+			.then((response) => {
+				
+				this.getCostCenter();
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	  }
+	handleTabChange = (event, value) => {
 		this.setState({ value });
+		this.setState({ [event.target.name]: event.target.value });
+
+	};
+	handleChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	
 	};
 	render() {
 		const { classes, theme } = this.props;
@@ -90,7 +261,7 @@ class CostCenter extends Component {
 						<AppBar position="static" color="default">
 							<Tabs
 								value={this.state.value}
-								onChange={this.handleChange}
+								onChange={this.handleTabChange}
 								indicatorColor="primary"
 								textColor="primary"
 								variant="fullWidth"
@@ -132,7 +303,7 @@ class CostCenter extends Component {
 											</TableRow>
 										</TableHead>
 										<TableBody>
-											{rows.map(row => (
+											{this.state.costcenter.map(row => (
 												<TableRow className={classes.row} key={row.id}>
 
 													<CustomTableCell align="center">{row.Code}</CustomTableCell>
@@ -140,10 +311,10 @@ class CostCenter extends Component {
 														{row.Description}
 													</CustomTableCell>
 													<CustomTableCell align="center" component="th" scope="row">
-														<IconButton className={classes.button} aria-label="Delete">
+														<IconButton className={classes.button} onClick={()=>this.deleteCostCenter(row.Id)} aria-label="Delete">
 															<DeleteIcon />
 														</IconButton>
-														<IconButton className={classes.button} aria-label="Edit">
+														<IconButton className={classes.button} onClick={()=>this.getCostCenterById(row.Id)} aria-label="Edit">
 															<EditIcon />
 														</IconButton>
 													</CustomTableCell>
@@ -155,35 +326,50 @@ class CostCenter extends Component {
 							</TabContainer>
 							<TabContainer dir={theme.direction}>
 								<form className={classes.container} noValidate autoComplete="off">
-									<TextField
-										id="outlined-name"
-										label="Code"
-										className={classes.textField}
-										value={this.state.name}
-										fullWidth
-										//   onChange={this.handleChange('name')}
-										margin="normal"
-										variant="outlined"
-									/>
-									<TextField
-										id="outlined-name"
-										label="Description"
-										fullWidth
-										className={classes.textField}
-										value={this.state.name}
-										//   onChange={this.handleChange('name')}
-										margin="normal"
-										variant="outlined"
-									/>
-										
+								
+								<Grid item xs={12} sm={5} style={{ marginRight: '5px' }}>
+										<TextField id="code" fullWidth label="Code" name="code"  value={this.state.code}  onChange={this.handleChange}
+						
+										/>
+										{this.validator.message('code', this.state.code, 'required')}
+									</Grid>
+									
+									<Grid item xs={12} sm={5} >
+										<TextField id="description" fullWidth label="description"  name="description"  value={this.state.description}  onChange={this.handleChange}
+										/>
+										{this.validator.message('description', this.state.description, 'required')}
+									</Grid>
+												<Grid item xs={12} sm={5} style={{ marginRight: '5px' }}   >
+										<FormControl className={classes.formControl}>
+											<InputLabel htmlFor="company">Company</InputLabel>
+											<Select
+												value={this.state.company}
+												onChange={this.handleChange}
+												inputProps={{
+													name: 'company',
+													id: 'company',
+												}}
+											>
+												<MenuItem value="">
+													<em>None</em>
+												</MenuItem>
+												{this.state.Companies.map(row => (
+													<MenuItem value={row.Id}>{row.CompanyName}</MenuItem>
+												))}
+											</Select>
+											{this.validator.message('company', this.state.company, 'required')}
+										</FormControl>
+									</Grid>
 								</form>
 								<div className="row">
+								<Grid item xs={12} sm={10} style={{ marginRight: '5px' }}   >
 									<div style={{float: "right","marginRight":"8px"}}>
 									
-									<Button variant="outlined" color="secondary" className={classes.button }>
-										Insert Record
+									<Button variant="outlined" color="secondary" onClick={this.insertUpdateCostCenter} className={classes.button }>
+												{this.state.Action}
       								</Button>
 									</div>
+									</Grid>
 								</div>
 							</TabContainer>
 						</SwipeableViews>
